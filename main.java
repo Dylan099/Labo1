280c977aad7ac041745a7e28328fd58bf521a314
Alejandro Antonio Martinez Fuertes <ale_mf_99@hotmail.com>
2019-08-30 01:17:38 -0400

    Fibonacci

--- main.java
+++ main.java
@@ -24,3 +24,21 @@ public static int factorialRecursivo(int num){
             return factorialRecursivo(num-1)*num;
         }
     }
+
+public static int fibonacci(int num){
+        if(num==1){
+            return 1;
+        }else if(num==0){
+            return 0;
+        }else{
+            int a=1;
+            int b=1;
+            int actual=0;
+            for(int i=2;i<num;i++){
+                actual=a+b;
+                b=a;
+                a=actual;
+            }
+            return actual;
+        }
+    }
